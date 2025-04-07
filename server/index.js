5fd293ff17a2087ad1b89204e7ccbdbfd1d69dcc
mynameist04st3r <t04st3r@mac.com>
2025-04-07 12:26:36 -0400

    Initialized project.

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const port = 8000;
+const express = require('express');
+const app = express();
+const knex = require('knex')(require('./knexfile.js')['development']);
+app.get('/', (req, res) => {
+  res.send('Server Operational.');
+})
+app.get('/dpwflow', (req, res) => {
+  knex('dpwflow').select('*').then(DATABASE_HEADER => {
+    let VARIABLE = DATABASE_HEADER.map(dpwflow => dpwflow.DATABASE_HEADER)
+    res.json(VARIABLE);
+  })
+});
+app.listen(port, () => {
+  console.log('Server running at http://localhost:' + port);
+});
