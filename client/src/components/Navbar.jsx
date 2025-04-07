8a40ef08287bbeda0d3748d2a8360c8751afdf31
Camilo <camilo.cueto@gmail.com>
2025-04-07 14:52:06 -0400

    created Navbar and other related components

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <nav className="nav">
+      <NavLink
+        to="/"
+        className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/launch"
+        className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+      >
+        Launch
+      </NavLink>
+
+      <NavLink
+        to="/maintenance-request"
+        className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+      >
+        Maintenance Request
+      </NavLink>
+      <NavLink
+        to="/dashboard"
+        className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+      >
+        Dashboard
+      </NavLink>
+      <NavLink
+        to="/maintenance-tracker"
+        className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+      >
+        Maintenance Tracker
+      </NavLink>
+      <NavLink
+        to="/my-requests"
+        className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+      >
+        My Requests
+      </NavLink>
+      <NavLink
+        to="/admin"
+        className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+      >
+        Admin
+      </NavLink>
+      <NavLink
+        to="/contact"
+        className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+      >
+        Contact
+      </NavLink>
+    </nav>
+  );
+}
+
+export default NavBar;
+
+// Launch.jsx
+// Maintenance_Request.jsx
+// Dashboard.jsx
+// Maintenance_Tracker.jsx
+// My_Requests.jsx
+// Admin.jsx
+// Contact.jsx
