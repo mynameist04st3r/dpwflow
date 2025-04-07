23f5cc7ecdffd15c4ce1db241c6b8bef89cedbb3
KierstenM29 <kikimorrow123@gmail.com>
2025-04-07 14:56:29 -0400

    Added clean dashboard page

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Dashboard</h1>
+      <p>This is the group dashboard. Period.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
