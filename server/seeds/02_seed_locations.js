39bd5016a22a9676d945ae1dbc3bce2507afb038
leviballew <levi.ballew10@gmail.com>
2025-04-07 12:53:57 -0400

    migrations and seeding with instructions

---
+++ server/seeds/02_seed_locations.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function(knex) {
+  await knex('locations').del();
+
+  await knex('locations').insert([
+    { id: 1, state: 'North Carolina', military_base: 'Fort Liberty' },
+    { id: 2, state: 'Virginia', military_base: 'Fort Eustis' },
+    { id: 3, state: 'Texas', military_base: 'Fort Cavazos' }
+  ]);
+
+  await knex.raw(`SELECT setval('locations_id_seq', (SELECT MAX(id) FROM locations))`);
+};
