d2c37ab0e365ea9434a4b170b6c1a6c3bfdc40ae
jdj-design <doc_jones12@yahoo.com>
2025-04-08 15:07:36 -0400

    added style sheet for activeRequest, routing to page on app.jsx

--- client/src/pages/ActiveRequest.jsx
+++ client/src/pages/ActiveRequest.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import "../styles/ActiveRequest.css";
 export default function ActiveRequest() {
   const [input, setInput] = useState("");
   const [result, setResult] = useState([]);
@@ -15,19 +15,25 @@ export default function ActiveRequest() {
 
   return (
     <div>
+      <div className="search-container">
+
       <input
+      className="search-input"
         placeholder="search active"
         value={input}
         onChange={(e) => setInput(e.target.value)}
-      />
+        />
 
       <div>
-        <button onClick={search}>Search</button>
+        <button
+        className="search-button" 
+        onClick={search}>Search</button>
       </div>
+        </div>
       <div>
         <ul>
           {result.map((active) => {
-            return <li key={active.id}>{active.name} </li>;
+            return <li key={active.id}>{active.id} </li>;
           })}
         </ul>
       </div>
