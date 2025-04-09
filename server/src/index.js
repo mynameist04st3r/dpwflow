08521e36775ee2fd0d5e43c8ba65dcf4a086393b
Camilo <camilo.cueto@gmail.com>
2025-04-09 16:47:55 -0400

    separated code and removed from NavBar.jsx into separate components, LoginForm.jsx, SignUpForm.jsx

--- server/src/index.js
+++ server/src/index.js
@@ -9,22 +9,25 @@ const { hash, compare } = require("@uswriting/bcrypt");
 const uuid = require("uuid");
 const session = require("express-session");
 const secretKey = uuid.v4();
-const requestsRoutes = require("./routes/requests");
-const getRequestsRoutes = require("./routes/GetRequests");
-const locationsRoutes = require("./routes/locations");
-const authRoutes = require("./routes/auth");
-
-require("dotenv").config();
 
 if (!process.env.NODE_ENV) {
   console.error("Missing NODE_ENV");
   process.exit(1);
 }
 
-// ======================
+const requestsRoutes = require("./routes/requests");
+const getRequestsRoutes = require("./routes/GetRequests");
+const locationsRoutes = require("./routes/locations");
+const authRoutes = require("./routes/auth");
 
-// Allows POST /login and /signup
-// Middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(
   session({
     secret: secretKey,
@@ -37,50 +40,14 @@ app.use(
   })
 );
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-app.use(express.json());
-
-app.listen(8000, () => {
-  console.log("Server running on http://localhost:8000");
-});
-
-// ======================
-
-app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-app.use(
-  session({
-    secret: secretKey,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      maxAge: 6000,
-    },
-  })
-);
-
 app.get("/", (req, res) => {
   res.send("Server operational");
 });
 
-// Routes
 app.use("/requests", requestsRoutes);
 app.use("/locations", locationsRoutes);
 app.use("/GetRequests", getRequestsRoutes);
-app.use("/", authRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(port, (req, res) => {
   console.log(`Your server is up at http://localhost:${port}/`);
