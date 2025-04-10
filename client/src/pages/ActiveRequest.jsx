1774ac931e1748257f9943bb57e9a0c5f1a769bf
jdj-design <doc_jones12@yahoo.com>
2025-04-10 12:03:28 -0400

    made link using home button to go back; added spans to break up results

--- client/src/pages/ActiveRequest.jsx
+++ client/src/pages/ActiveRequest.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "../styles/ActiveRequest.css";
+import { useNavigate } from "react-router-dom";
 
 export default function ActiveRequest() {
   const [input, setInput] = useState("");
   const [filteredResults, setFilteredResults] = useState([]);
+  const navigate = useNavigate();
 
   // Fetch data when the Search button is clicked
   const handleSearch = async () => {
@@ -19,6 +21,7 @@ export default function ActiveRequest() {
     } catch (error) {
       console.error("fetch error", error);
     }
+    
   };
 
   return (
@@ -26,7 +29,7 @@ export default function ActiveRequest() {
 
     <div className="container">
     <div className="container_header">
-        <button>home</button>
+        <button onClick={()=> navigate('./launch')}>home</button>
     </div>
       <h1>Accepted Work Orders</h1>
       <div className="search-container">
@@ -51,7 +54,12 @@ export default function ActiveRequest() {
       .filter((item) => item.accepted === true) // Filter accepted requests
       .map((item, index) => (
         <li key={index} className="list">
-          Building Number {item.building_number} |Room Number {item.room_number} <br></br>Discription: {item.work_order_desc}
+          <div className="toprow">
+            <span> Building Number: {item.building_number} </span>
+            <span>Room Number {item.room_number}</span>
+            <span>Date created {item.date_created}</span>
+          </div>
+          Discription: {item.work_order_desc}
         </li>
       ))}
   </ul>
