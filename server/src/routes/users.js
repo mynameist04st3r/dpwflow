39aa31487f8bd1d8082d9527227d981b2995b607
leviballew <levi.ballew10@gmail.com>
2025-04-10 17:14:40 -0400

    Hashed seed data user passwords, added users endpoints

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,31 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('knex')(require('../../knexfile')[process.env.NODE_ENV || 'development']);
-
-
-// Create user
-// app.post("/users", async (req, res) => {
-//   const { username } = req.body;
-
-//   if (!username) {
-//     return res.status(400).json({ error: "Username is required" });
-//   }
-
-//   try {
-//     const result = await pool.query(
-//       "INSERT INTO users (username) VALUES ($1) RETURNING *",
-//       [username]
-//     );
-//     res.status(201).json(result.rows[0]);
-//   } catch (err) {
-//     if (err.code === "23505") {
-//       return res.status(409).json({ error: "Username already exists" });
-//     }
-//     console.error("Create user error:", err);
-//     res.status(500).json({ error: "Failed to create user" });
-//   }
-// });
-
 const { hash } = require('@uswriting/bcrypt');
 
 
@@ -88,7 +63,7 @@ router.patch('/:id/role', async (req, res) => {
   }
 });
 
-router.patch('/users/:id/credentials', async (req, res) => {
+router.patch('/:id/credentials', async (req, res) => {
   const { id } = req.params;
   const { username, password } = req.body;
 
