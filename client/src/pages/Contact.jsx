1d82569600f5829f099fcbf21df79ea4f7c5e5a1
Camilo <camilo.cueto@gmail.com>
2025-04-15 13:38:02 -0400

    updated css on Active Request, Contact, Admin, Maintenance Request and User profile to add uniformity

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from "react";
+import "../styles/Contact.css";
 
 export default function Contact() {
   return (
-    <div>
+    <div className="section-container">
       <h1>Contact Us!</h1>
 
       <h2>For EMERGENCY work orders call 7108675209:</h2>
@@ -11,36 +12,14 @@ export default function Contact() {
       <h3>Building Manager(s):</h3>
 
       <p>*** Pulls user data by location ***</p>
-      
-      <p>$Rank $LastName, $FirstName:    
-      $phoneNumber
-      $email
-      </p>
 
-      <p>Rank $LastName, $FirstName:     
-      $phoneNumber
-      $email
+      <p>$Rank $LastName, $FirstName: $phoneNumber $email</p>
 
-      </p>
-      <p>Rank $LastName, $FirstName:     
-      $phoneNumber
-      $email
-
-      </p>
-      <h3>
-        
-        
-        Admin:</h3>
-        <p>
-      $FirstName $LastName: 
-      $phoneNumber
-      $email
-
-      </p>
-      <h4>
-      Site issues and questions:  email: Team BLB
-      </h4>
-
+      <p>Rank $LastName, $FirstName: $phoneNumber $email</p>
+      <p>Rank $LastName, $FirstName: $phoneNumber $email</p>
+      <h3>Admin:</h3>
+      <p>$FirstName $LastName: $phoneNumber $email</p>
+      <h4>Site issues and questions: email: Team BLB</h4>
     </div>
-  )
+  );
 }
